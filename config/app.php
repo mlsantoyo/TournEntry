f94d8d15c4c9e80254ed9f7cc16757fe3c74ac1f
Marcy <mlsantoyo@att.net>
2017-10-26 15:51:55 -0400

    config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://calm-everglades-23342.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
